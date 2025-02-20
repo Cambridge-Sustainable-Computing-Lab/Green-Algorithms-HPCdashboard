e87000d6b0df0d03ecdc0557e4c77e14dfc29f1c
mattagape <mg2216@cam.ac.uk>
2025-02-20 10:20:21 +0000

    Added small utility script for generating pickled test data.

---
+++ ga_dashboard/tests/pickle_it.py
@@ -0,0 +1,12 @@
+# Run this from GA4HPCdashboard/ga_dashboard directory:
+# python tests/pickle_it.py
+
+import pickle
+
+# Add the module's path:
+import sys
+sys.path.append('src')
+from ga_dashboard.backend.utils import simulate_mock_jobs
+
+df2 = simulate_mock_jobs()
+df2.to_pickle("tests/testdata/df_agg_X_mockMultiUsers_1.pkl")
