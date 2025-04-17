7fb63bb72e2a33379ee7980eb72a18b8fd4f8a01
mattagape <mg2216@cam.ac.uk>
2025-04-17 18:12:13 +0100

    Now we have command-line arguments.

--- scripts/backend/run_sacct_only.py
+++ scripts/backend/run_sacct_only.py
@@ -1,60 +1,94 @@
 # sacct_file_pull.py
 #
-# Run the sacct command on a remote server, store the results in a file, then download that file
-# to the local machine where this script is running.
+# Run the sacct command on a remote HPC server and store the results in a file.
+# That file can be downloaded later, and passed to run_backend.sh with --useCustomLogs argument.
+#
+# You will want to use this script if, for example, you cannot run the rest of the Python code
+# on that remote server.
+#
+# Example: 
+# python run_sacct_only.py -S 2025-04-14 -E 2025-04-18 -a -o my_sacct_file.txt 
 
+import argparse
+import datetime
 import logging
 import subprocess
 
 logger = logging.getLogger(__name__)
-logging_level = logging.DEBUG  # Select logging.INFO or logging.DEBUG as required.
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging_level)
+
+def create_arguments():
+    """
+    Command line arguments for the script.
+    :return: argparse object
+    """
+    parser = argparse.ArgumentParser(description='Run sacct command and save its output to a file.')
+
+    default_endDay = datetime.date.today().strftime("%Y-%m-%d")  # today
+
+    # Timeframe
+    parser.add_argument('-S', '--startDay', type=str, required=True,
+                        help='The first day to take into account, as YYYY-MM-DD.')
+    parser.add_argument('-E', '--endDay', type=str,
+                        help='The last day to take into account, as YYYY-MM-DD (default: today).',
+                        default=default_endDay)
+    
+    # Output file
+    parser.add_argument('-o', '--outFile', type=str, default='', required=True,
+                        help='The name of the file to be written, for storing the output of sacct.')
+
+    # (Optional) run sacct for all users
+    parser.add_argument("-a", "--allUsers", help='Run sacct for all users (probably requires admin rights).', required=False, dest='for_all_users', action='store_true')
+
+    # (Optional) run as debug
+    parser.add_argument("-d", "--debug", help='Debug mode', required=False, dest='debug', action='store_true')
+
+    args = parser.parse_args()
+    return args
 
 
-def capture_sacct_output():
+def capture_sacct_output(args):
 
-    # We want to run the sacct command on the remote server, save the result in a file on the server,
-    # then download it and use it.
-    #
-    # Alternatively, we could ssh into the server, run the sacct command, disconnect, and save the data locally
+    # We want to run the sacct command on the remote HPC machine and save the result in a file on that machine.
+    # It can then be downloaded for use with run_backend.py/.sh
 
     bash_com = [
         "sacct",
         "--starttime",
-        "2025-04-14",  #self.args.startDay,  # format YYYY-MM-DD 2025-04-14 --endtime 2025-04-15
+        args.startDay,  # format YYYY-MM-DD
         "--endtime",
-        "2025-04-18",  #self.args.endDay,  # format YYYY-MM-DD
+        args.endDay,  # format YYYY-MM-DD
         "--format",
         "UID,User,JobID,JobName,Submit,Elapsed,Partition,NNodes,NCPUS,TotalCPU,CPUTime,ReqMem,MaxRSS,WorkDir,State,Account,AllocTres",
         "-P",
         "-L"  # All clusters
     ]
 
-    filename = "matt_sacct.txt"
+    filename = args.outFile
 
-    use_as_admin = True
-                
-    if use_as_admin:
+    if args.for_all_users:
         bash_com.append('--allusers')
-
 
     logger.debug("info SACCT CMD: " + str(bash_com))
 
-    completed_processs = subprocess.run(bash_com, capture_output=True)  #, text=True)
+    completed_processs = subprocess.run(bash_com, capture_output=True)
     data = completed_processs.stdout
     logger.debug("Output captured:")
     logger.debug(data)
 
-    # Save the binary data into a file.
-    with open(filename, 'wb') as f: ## File
-        f.write(data)
-
+    # Save the binary data into a file, for later ingestion by run_backend.py/.sh
+    with open(filename, 'wb') as myfile:
+        myfile.write(data)
 
 
 def main():
+    print("") # For neater output!
 
-    capture_sacct_output()
+    args = create_arguments()
 
+    logging_level = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging_level)
+    
+    capture_sacct_output(args)
 
 
 if __name__ == "__main__":
