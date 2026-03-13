1eef7dc21c61a575dffc66c347271295c9f320ab
Navirah Kamal <70336688+Navirah@users.noreply.github.com>
2026-03-13 15:14:04 +0000

    Refactor variable names for clarity in slurm.py

--- ga_dashboard/backend/workload_manager/slurm.py
+++ ga_dashboard/backend/workload_manager/slurm.py
@@ -365,7 +365,7 @@ class SlurmManager(SlurmBase):
             self.logs_df['Account_'] = ''
 
         ### Aggregate per jobID
-        self.df_agg = self.logs_df.groupby('single_jobID').agg({
+        self.df_agg_0 = self.logs_df.groupby('single_jobID').agg({
             'TotalCPUtime_': 'max',
             'CPUwallclocktime_': 'max',
             'WallclockTimeX': 'max',
@@ -489,10 +489,10 @@ class SlurmManager(SlurmBase):
 
         self.unfinished_jobs = grouped_by_job
 
-    def filter_and_store_unfinished_jobs(self, unfin_jobs_service: UnfinishedJobsService):
+    def filter_and_store_unfinished_jobs(self, unfinished_jobs_service: UnfinishedJobsService):
         '''
             Use the UnfinishedJobsService to filter out unfinished jobs from the logs dataframe, and store them in the database.
         '''
-        self.unfinished_jobs, self.logs_df = unfin_jobs_service.filter_unfinished_jobs(self.logs_df)
+        self.unfinished_jobs, self.logs_df = unfinished_jobs_service.filter_unfinished_jobs(self.logs_df)
         self.select_distinct_unfinished_jobs()
-        unfin_jobs_service.save_unfinished_jobs(self.unfinished_jobs)
+        unfinished_jobs_service.save_unfinished_jobs(self.unfinished_jobs)
