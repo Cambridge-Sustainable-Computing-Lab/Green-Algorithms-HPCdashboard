ea8f88926443109c929da10e313b702825516622
Navirah Kamal <navirahkamal@gmail.com>
2026-07-14 13:21:49 +0100

    updated mock jobs generation and deleted old mock files

--- tests/pickle_it.py
+++ tests/pickle_it.py
@@ -1,10 +1,40 @@
-# Run this from GA4HPCdashboard/ga_dashboard directory:
-# python tests/pickle_it.py
+# ------------------------------------------------------------------
+# Mock Data Generator for GA4HPC Dashboard Tests
+# This script generates a synthetic enriched logs dataset. i.e. with energy consumption and carbon footprint data,
+# simulating job records for multiple mock users. 
+# The generated data is saved as a pickle file to serve as static test data for backend dashboard unit tests.
+#
+# Run this from the project root:
+#    $ python tests/pickle_it.py --workload_manager slurm
+# ------------------------------------------------------------------
 
-# Add the module's path:
-import sys
-sys.path.append('src')
-from ga_dashboard.backend.helpers.utils import simulate_mock_jobs
+import argparse
+from ga_dashboard.backend.helpers import utils
 
-df2 = simulate_mock_jobs()
-df2.to_pickle("tests/testdata/df_agg_X_mockMultiUsers_1.pkl")
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(
+        description="Generate and pickle synthetic, enriched HPC job logs for dashboard testing."
+    )
+    argparser.add_argument(
+        "--workload_manager", 
+        help="Name of the workload manager to simulate (e.g., 'slurm')", 
+        required=True, 
+        metavar="WORKLOAD_MANAGER", 
+        dest="workload_manager"
+    )
+
+    args = argparser.parse_args()
+    wm = args.workload_manager.lower()
+
+    match wm:
+        case "slurm":
+            print("Simulating mock enriched Slurm jobs...")
+            df2 = utils.simulate_mock_enriched_jobs()
+            
+            # Save the simulated data to a test fixture
+            output_path = "tests/testdata/df_enriched_mockMultiUsers.pkl"
+            df2.to_pickle(output_path)
+            print(f"Success! Mock data saved to: {output_path}")
+            
+        case _:
+            print(f"Error: Workload manager '{args.workload_manager}' is not recognized or supported.")
